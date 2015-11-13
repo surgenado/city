c9efeef751f5ab812ac524578a64ca57790c5bf9
Kirtley <kirtley@wienbroer.com>
2015-11-13 22:05:16 +0000

    modified:   _scripts/update.sh

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -4,13 +4,14 @@ TEMPLATE=YYYY-MM-DD-surgenado.md
 DATE=date
 FORMAT="%Y-%m-%d"
 today=`$DATE +$FORMAT`
+tomorrow=`$DATE +$FORMAT -d "$now + 1 day"`
 yesterday=`$DATE +$FORMAT -d "$now - 1 day"`
-next_week=`$DATE +$FORMAT -d "$now + 1 week"`
+next_week=`$DATE +$FORMAT -d "$tomorrow + 1 week"`
 FILE2ADD=$next_week"-surgenado.md"
 FILE2REMOVE=$yesterday"-surgenado.md"
 
-# mv ../_drafts/$FILE2ADD ../_posts/$FILE2ADD
-# rm ../_posts/$FILE2REMOVE
+mv ../_drafts/$FILE2ADD ../_posts/$FILE2ADD
+rm ../_posts/$FILE2REMOVE
 
 echo $FILE2ADD
 echo $FILE2REMOVE
