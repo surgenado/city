ce7c50afbafc84ee590733ea2dce8e2565ddddc9
Kirtley <kirtley@wienbroer.com>
2015-12-04 16:49:25 -0800

    .

--- _scripts/update_posts.sh
+++ _scripts/update_posts.sh
@@ -5,12 +5,12 @@ FORMAT="%Y-%m-%d"
 today=`$DATE +$FORMAT`
 tomorrow=`$DATE +$FORMAT -d "$now + 1 day"`
 yesterday=`$DATE +$FORMAT -d "$now - 1 day"`
-next_week=`$DATE +$FORMAT -d "$tomorrow + 1 week"`
-FILE2ADD=$next_week"-surgenado.md"
-FILE2REMOVE=$yesterday"-surgenado.md"
+last_week=`$DATE +$FORMAT -d "$yesterday - 1 week"`
+FILE2ADD=$tomorrow"-SURGENADO.md"
+FILE2REMOVE=$last_week"-SURGENADO.md"
 
 mv ../_drafts/$FILE2ADD ../_posts/$FILE2ADD
-rm ../_posts/$FILE2REMOVE
+mv ../_posts/$FILE2REMOVE ../_past/$FILE2REMOVE
 
 echo $FILE2ADD
 echo $FILE2REMOVE
